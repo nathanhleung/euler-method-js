0ad5e61b269c870cff2a6c897d5836c8c8400cf5
Nathan Leung <nathanhleung@users.noreply.github.com>
2016-12-20 08:00:36 -0500

    add initialx/y

--- index.js
+++ index.js
@@ -13,18 +13,19 @@
 
   solveBtn.addEventListener('click', () => {
   	const equation = val('equation');
-    const initial = Number(val('initial'));
+    const initialX = Number(val('initialX'));
+    const initialY = Number(val('initialY'));
     const steps = Number(val('steps'));
     const target = Number(val('target'));
-    const deltaX = target / steps;
-    let result = initial;
+    const deltaX = (target - initialX) / steps;
+    let result = initialY;
     const xTable = [];
     const yTable = [];
     const dyDxTable = [];
     const dyTable = [];
   	for (let i = 0; i <= steps; i++) {
       const dyDx = math.eval(equation, {
-      	x: i * deltaX,
+      	x: initialX + i * deltaX,
         y: result,
       });
       xTable.push(i * deltaX);
